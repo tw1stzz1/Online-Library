a9d674470f5e187891a4c81933a52ee0c28a6d27
Tkaz <egor4ig95@mail.ru>
2024-04-20 14:46:40 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,22 +1,32 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
+from livereload import Server, shell
 
 
-with open("books_parameters.json", "r", encoding="utf8") as my_file:
-    books_parameters = my_file.read()
+def on_reload():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-books = json.loads(books_parameters)
+    template = env.get_template('template.html')
 
-rendered_page = template.render(
-    books=books
-)
+    with open("books_parameters.json", "r", encoding="utf8") as my_file:
+        books_parameters = my_file.read()
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    books = json.loads(books_parameters)
+
+    rendered_page = template.render(
+        books=books
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+
+on_reload()
+
+server = Server()
+server.watch('template.html', on_reload())
+server.serve(root='.')
