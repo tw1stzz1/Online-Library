92ae020948b2bb28bbf0eab8604e1c9fec4bb1a8
Tkaz <egor4ig95@mail.ru>
2024-05-24 19:10:46 +0300

    update

--- render_website.py
+++ render_website.py
@@ -1,6 +1,7 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
 from more_itertools import chunked
+from pathlib import Path
 
 from livereload import Server, shell
 
@@ -17,13 +18,19 @@ def on_reload():
         books_parameters = my_file.read()
 
     books = json.loads(books_parameters)
-    sorted_books = list(chunked(books, 2))
-    rendered_page = template.render(
-        books=sorted_books
-    )
+    books_pages = list(chunked(books, 20))
+    pages_count = len(books_pages)
+    Path('pages').mkdir(parents=True, exist_ok=True)
+    for page_number, books_on_page in enumerate(books_pages, 1):
+        sorted_books = list(chunked(books_on_page, 2))
+        rendered_page = template.render(
+            pages=pages_count,
+            page_number=page_number,
+            books=sorted_books
+        )
 
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+        with open(f'pages/index{page_number}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 on_reload()
